97516e6e7fcabadc00d31564da37feaa08d595b1
NehalBalakumar <nehalbalakumar7@gmail.com>
2026-02-17 01:18:52 +0530

    Initial backend setup with MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+// MongoDB Connect
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected ");
+})
+.catch((err) => {
+  console.log("MongoDB Connection Error ", err);
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Society SaaS Backend Running ",
+    status: "success"
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
