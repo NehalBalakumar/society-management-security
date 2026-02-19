23cbd926c70c1b31c8e294e11ef0d566855bd0a1
NehalBalakumar <nehalbalakumar7@gmail.com>
2026-02-20 00:21:21 +0530

    Added register API with password hashing

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // MongoDB Connect
 mongoose.connect(process.env.MONGO_URI)
